2791db439efe9ed6ae8eb4237b7734d2669f74ac
Samoei <samoeiayuka@gmail.com>
2021-02-23 15:26:21 +0200

    Find second largest no in arra

---
+++ SecondLargestNum.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    var distinctNums = [...new Set(nums)]; //create new array with unique values only
+    distinctNums.splice(distinctNums.indexOf(Math.max(...distinctNums)), 1);//remove first max
+    
+    return Math.max(...distinctNums);//return second max
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
